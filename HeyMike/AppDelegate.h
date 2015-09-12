2a3917e5f255ddebd9147bf966788b2cbaab9ee8
mlee3223 <stat2315@gmail.com>
2015-09-12 13:06:54 -0400

    Initial Commit

---
+++ HeyMike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeyMike
+//
+//  Created by Michael Lee on 9/12/15.
+//  Copyright (c) 2015 MoveMen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeyMike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeyMike
+//
+//  Created by Michael Lee on 9/12/15.
+//  Copyright (c) 2015 MoveMen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
